9eab88b747fa3b4acadb0c200175105beeca641a
Dark-LYNN <botdevlynn@gmailcom>
2024-12-22 12:36:05 +0100

    Added cors for /request.

--- src/routes/other/request.ts
+++ src/routes/other/request.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/',
   (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   },
