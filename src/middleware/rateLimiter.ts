550902e7a2f91806380c6307ed8c99ce123e27c9
Dark-LYNN <botdevlynn@gmailcom>
2024-12-25 13:38:24 +0100

    Changed Ratelimits

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -2,27 +2,27 @@ import rateLimit from 'express-rate-limit';
 import { Request } from 'express';
 
 /**
- * Get Request rateLimit 30 per minute
+ * Get Request rateLimit 5 per seconde
  */
 export const rateLimiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 10, // Max 10 Requests per minute
+  windowMs: 1000,
+  max: 5, // Max 5 Requests per seconde
   message: { message: 'Too many requests, please try again later.' },
   keyGenerator: (req: Request) => {
-    const ip = req.ip || '';
-    return ip;
+    const realIP = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
+    return Array.isArray(realIP) ? realIP[0] : realIP || "";
   },
 });
 
 /**
- * Post Request rateLimit 1 per minute
+ * Post Request rateLimit 1 per seconde
  */
 export const postRateLimiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 1, // Max 1 Requests per minute
+  windowMs: 1000,
+  max: 1, // Max 1 Requests per seconde
   message: { message: 'Too many requests, please try again later.' },
   keyGenerator: (req: Request) => {
-    const ip = req.ip || '';
-    return ip;
+    const realIP = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
+    return Array.isArray(realIP) ? realIP[0] : realIP || "";
   },
 })
