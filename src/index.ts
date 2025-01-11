f280d4208054e56b4419962220949d9eb31ccf1a
Dark-LYNN <botdevlynn@gmailcom>
2025-01-11 15:01:48 +0100

    TESTING

--- src/index.ts
+++ src/index.ts
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   if (req.method === 'POST' || req.method === 'PUT') {
     validateIPMiddleware(req, res, next);
+    return;
   }
   next();
 });
