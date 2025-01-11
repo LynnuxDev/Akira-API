d7c46196928ca92084815260187b8441bd15e370
Dark-LYNN <botdevlynn@gmailcom>
2025-01-11 14:04:46 +0100

    TESTING

--- src/middleware/validateIP.ts
+++ src/middleware/validateIP.ts
@@ -8,11 +8,19 @@ const allowedIPs = ['127.0.0.1', `${process.env.AKIRA_IP}`]; // TODO: get this l
  * @returns 403 Error if user isn't allowed to use path.
  */
 export const validateIPMiddleware = (req: Request, res: Response, next: NextFunction): void | Response => {
-  const requestIP = req.headers['x-real-ip'] || req.ip || req.connection.remoteAddress;
-  const normalizedIP = Array.isArray(requestIP) ? requestIP.pop() : requestIP;
+  const requestIP = req.headers['x-real-ip'] ||
+    (typeof req.headers['x-forwarded-for'] === 'string' ? req.headers['x-forwarded-for'].split(',')[0] : '') ||
+    req.ip ||
+    req.connection.remoteAddress;
+
+  let normalizedIP = Array.isArray(requestIP) ? requestIP.pop() : requestIP;
 
   logger.info('Request IP:', requestIP);
   logger.info('Normalized IP:', normalizedIP);
+
+  if (normalizedIP && normalizedIP.includes(':')) {
+    normalizedIP = normalizedIP.split(':')[0];
+  }
 
   if (allowedIPs.includes(normalizedIP || '')) {
     next();
