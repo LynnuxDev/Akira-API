566f33475436098b0756045172a9a45c3a9d6746
Dark-LYNN <botdevlynn@gmailcom>
2025-01-11 15:41:04 +0100

    Thank you very much sir i thank you with all my heart, thank you for allowing my code to work.

--- src/middleware/validateIP.ts
+++ src/middleware/validateIP.ts
@@ -1,7 +1,6 @@
 import { logger, getClientIP } from '../utils';
 import { Request, Response, NextFunction } from 'express';
 
-
 /**
  * Check if user is a valid user.
  * @returns 403 Error if user isn't allowed to use path.
@@ -10,13 +9,11 @@ export const validateIPMiddleware = (req: Request, res: Response, next: NextFunc
   const normalizedIP = getClientIP(req);
   const allowedIPs = ['127.0.0.1', `${process.env.AKIRA_IP}`];
 
-  console.log('Normalized IP for validation:', normalizedIP);
-  console.log('Allowed IPs:', allowedIPs);
-  console.log('AKIRA_IP from env:', process.env.AKIRA_IP);
-
   if (allowedIPs.includes(normalizedIP || '')) {
+    logger.info(`Request from ${normalizedIP} passed.`)
     next();
   } else {
+    logger.info(`Request from ${normalizedIP} forbidden.`)
     res.status(403).json({ message: 'Forbidden' });
   }
 };
